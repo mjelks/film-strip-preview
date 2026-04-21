3887fe560776f07835285b9edf9767c330c393e1
mjelks <spam@zaftig.net>
2026-04-21 16:41:14 -0700

    Initial Commit

---
+++ filmStripPreview/filmStripPreviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  filmStripPreviewApp.swift
+//  filmStripPreview
+//
+//  Created by Michael Jelks on 4/21/26.
+//
+
+import SwiftUI
+
+@main
+struct filmStripPreviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
